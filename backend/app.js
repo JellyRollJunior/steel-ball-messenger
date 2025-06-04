282c6579c7346bd3e79cc6e2decea756110d8204
Brandon Lin <brandonlin9716@gmail.com>
2025-06-04 14:09:54 -0700

    Implement Post /users to create users

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routes/userRouter.js';
 
 const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
     res.json('hello world!')
 })
+app.use('/users', userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
