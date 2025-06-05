5da4b3d429fd7426d129b62a49fef11b9b052845
Brandon Lin <brandonlin9716@gmail.com>
2025-06-04 18:58:41 -0700

    Control signup inputs

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+
 const Signup = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const submitSignup = async (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={submitSignup}>
       <label htmlFor="username">Username</label>
-      <input type="text" name="password" id="username" />
+      <input
+        type="text"
+        name="username"
+        id="username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        minLength={6}
+        maxLength={24}
+      />
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        value={password}
+        on={(event) => setPassword(event.target.value)}
+        minLength={6}
+        maxLength={24}
+      />
       <button>Submit</button>
     </form>
   );
