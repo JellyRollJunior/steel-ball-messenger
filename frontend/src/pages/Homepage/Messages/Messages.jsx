9d45ba06dd075c43c303927bd4fff8c85918b001
Brandon Lin <brandonlin9716@gmail.com>
2025-06-12 12:48:14 -0700

    Create input for submitting messages

--- frontend/src/pages/Homepage/Messages/Messages.jsx
+++ frontend/src/pages/Homepage/Messages/Messages.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { useMessages } from '../../../hooks/useMessages.js';
 
 const Messages = ({ chatId }) => {
   const { messages, isLoading, error } = useMessages(chatId);
+  const [content, setContent] = useState('');
+
+  const createChat = async () => {};
 
   if (!chatId) {
     return <h2>Messages</h2>;
@@ -13,15 +17,29 @@ const Messages = ({ chatId }) => {
       {error && <h2>{error}</h2>}
       {isLoading && <h2>loading chats</h2>}
       {messages && (
-        <ul>
-          {messages.map((message) => (
-            <li key={message.id}>
-              <div>{message.content}</div>
-              <div>{message.sender.id} {message.sender.username}</div>
-              <div>{message.sendTime}</div>
-            </li>
-          ))}
-        </ul>
+        <>
+          <ul>
+            {messages.map((message) => (
+              <li key={message.id}>
+                <div>{message.content}</div>
+                <div>
+                  {message.sender.id} {message.sender.username}
+                </div>
+                <div>{message.sendTime}</div>
+              </li>
+            ))}
+          </ul>
+          <form onSubmit={createChat}>
+            <input
+              type="text"
+              name='content'
+              value={content}
+              onChange={(event) => setContent(event.target.value)}
+              required
+            />
+            <button>send</button>
+          </form>
+        </>
       )}
     </section>
   );
