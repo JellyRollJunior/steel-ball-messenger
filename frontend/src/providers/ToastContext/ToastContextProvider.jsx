a696987bb8b28a2dddcff494f796be2117aa2e5a
Brandon Lin <brandonlin9716@gmail.com>
2025-07-08 16:17:53 -0700

    Persist toasts for 2s

--- frontend/src/providers/ToastContext/ToastContextProvider.jsx
+++ frontend/src/providers/ToastContext/ToastContextProvider.jsx
@@ -9,23 +9,27 @@ const ToastContextProvider = ({ children }) => {
       id: id,
       content: content,
       isError: isError,
-    }
-  }
+    };
+  };
 
-  const createToast = (content) => {
+  const createToast = async (content) => {
     const id = crypto.randomUUID();
-    setToasts((prev) => [
-      ...prev,
-      createToastObject(id, content, false),
-    ]);
-  }
+    setToasts((prev) => [...prev, createToastObject(id, content, false)]);
 
+    // persist for 2s
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    deleteToast(id);
+  };
+
+  const deleteToast = (id) => {
+    setToasts((prev) => prev.filter((toast) => toast.id != id));
+  };
 
   return (
     <ToastContext.Provider value={{ toasts, createToast }}>
       {children}
     </ToastContext.Provider>
-  )
+  );
 };
 
-export { ToastContextProvider }
+export { ToastContextProvider };
