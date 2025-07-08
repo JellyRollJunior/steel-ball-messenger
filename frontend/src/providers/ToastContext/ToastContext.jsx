7df8ff6093245273d2997e6f1c32ec7c92e2e83c
Brandon Lin <brandonlin9716@gmail.com>
2025-07-08 16:25:17 -0700

    Implement creating error toast. Refactor toast creation for DRY

--- frontend/src/providers/ToastContext/ToastContext.jsx
+++ frontend/src/providers/ToastContext/ToastContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react';
 const ToastContext = createContext({
   toasts: [],
   createToast: () => {},
-  createToastError: () => {},
+  createErrorToast: () => {},
   deleteToast: () => {},
 });
 
--- frontend/src/providers/ToastContext/ToastContextProvider.jsx
+++ frontend/src/providers/ToastContext/ToastContextProvider.jsx
@@ -4,21 +4,30 @@ import { ToastContext } from './ToastContext.jsx';
 const ToastContextProvider = ({ children }) => {
   const [toasts, setToasts] = useState([]);
 
-  const createToastObject = (id, content, isError) => {
-    return {
-      id: id,
-      content: content,
-      isError: isError,
-    };
+  const addToastToList = (content, isError) => {
+    const id = crypto.randomUUID();
+    setToasts((prev) => [
+      ...prev,
+      {
+        id: id,
+        content: content,
+        isError: isError,
+      },
+    ]);
+
+    return id;
   };
 
   const createToast = async (content) => {
-    const id = crypto.randomUUID();
-    setToasts((prev) => [...prev, createToastObject(id, content, false)]);
+    const id = addToastToList(content, false);
 
     // persist for 2s
     await new Promise((resolve) => setTimeout(resolve, 2000));
     deleteToast(id);
+  };
+
+  const createErrorToast = async (content) => {
+    addToastToList(content, true);
   };
 
   const deleteToast = (id) => {
@@ -26,7 +35,9 @@ const ToastContextProvider = ({ children }) => {
   };
 
   return (
-    <ToastContext.Provider value={{ toasts, createToast }}>
+    <ToastContext.Provider
+      value={{ toasts, createToast, createErrorToast, deleteToast }}
+    >
       {children}
     </ToastContext.Provider>
   );
